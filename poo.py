2b934839d58bca50866575e39461452497d99306
Lucas Bulaccios <133404051+HipolitoYrigoyen@users.noreply.github.com>
2024-03-06 15:59:31 -0300

    poo.py

--- poo.py
+++ poo.py
@@ -1,44 +1,37 @@
    ## crea una clase llamda vehiculo capsa de procesar la informacion basica d los autos en una consecionaria. Debera tener constructor y los atributos del vehiculo serán: patente, goma, marca, modelo, kilometraje. Crear metodos (accesos) getter y (modifiacion) setter. mopstrar por pantalla al menos 1 atrubuto y modificar el kilometrajes
 
-public class Vehiculo {
-   String Patente;
-   String Goma;
-   String Marca;
-   String Modelo;
-   String Kilometraje;
-
-
-
-public Vehiculo (String Patente, String Goma, String Marca, String Modelo, String Kilometraje){
-   this.patente=patente;
-   this.goma=goma;
-   this.marca=marca;
-   this.modelo=modelo;
-   this.kilometraje=kilometraje;
- }
-}
-
-
-class Vehiculo{ 
-   constructor(kilometraje){
-   this.kilometraje = kilometraje;
-}
-get kilometraje() {
-return this.kilometraje;
-}
-set kilometraje(NuevoKilometraje){ 
-this.kilometraje = NuevoKilometraje;
- }
-}
-   
-
-
-
 #patente
 #goma
 #marca
 #modelo
 #kilometraje
+
+class Vehiculo:
+    def __init__(self, patente, marca, kilometraje):
+        self.patente = patente
+        self.marca = marca
+        self.kilometraje = kilometraje
+
+    def get_patente(self):
+        return self.patente
+    def set_patente(self, patente):
+        self.patente = patente
+    def get_marca(self):
+        return self.marca
+    def set_marca(self, marca):
+        self.marca = marca
+    def get_kilometraje(self):
+        return self.kilometraje
+    def set_kilometraje(self, kilometraje):
+        self.kilometraje = kilometraje
+    def mostrar_kilometraje(self):
+        print("el kilometraje es de:", self.kilometraje)
+
+
+vehiculo = Vehiculo("A200 Sedan", "Mercedes Benz", 12400)
+print("El vehiculo es de la marca:", vehiculo.get_marca())
+vehiculo.mostrar_kilometraje()
+
 
 
 
